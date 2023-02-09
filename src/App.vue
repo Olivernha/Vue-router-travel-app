188c3713e6a9b1b8af9dc3c597c86c8d2c7da3ca
Nay Htet Aung <nayhtetaung201733@gmail.com>
2023-02-09 20:08:02 +0630

    setting up with vue-router

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
 </script>
 
 <template>
-
+      <img alt="Vue logo" src="./assets/vue.svg" />
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <br />
+      <router-view></router-view>
 </template>
 
 <style scoped>
