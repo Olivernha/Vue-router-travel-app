65eb531780ce1bbeab5e0b811bcd86c74a3b7f23
Nay Htet Aung <nayhtetaung201733@gmail.com>
2023-02-10 20:10:49 +0630

    lazy loading routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,44 +1,35 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 import Home from "@/views/Home.vue";
-import About from "@/views/About.vue";
-import Brazil from "@/views/Brazil.vue";
-import Jamacia from "@/views/Jamaica.vue";
-import Hawaii from "@/views/Hawaii.vue";
-import Panama from "@/views/Panama.vue";
+
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: About,
-  },
-  {
-    path: '/brazil',
-    name: 'Brazil',
-    component: Brazil
-  },
-  {
-    path:'/hawaii',
-    name:'Hawaii',
-    component: Hawaii
-  },
-  {
-    path:'/jamaica',
-    name:'Jamacia',
-    component: Jamacia
-  },
-  {
-    path:'/panama',
-    name:'Panama',
-    component: Panama
-  }
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: '/brazil',
+        name: 'Brazil',
+        component: () => import('@/views/Brazil.vue')
+    },
+    {
+        path: '/hawaii',
+        name: 'Hawaii',
+        component: () => import('@/views/Hawaii.vue')
+    },
+    {
+        path: '/jamaica',
+        name: 'Jamaica',
+        component: () => import('@/views/Jamaica.vue')
+    },
+    {
+        path: '/panama',
+        name: 'Panama',
+        component: () => import('@/views/Panama.vue')
+    }
 ];
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 });
 export default router;
