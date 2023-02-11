f4a4c5c01a521f3e4e94748dd46028c7f0efc308
Nay Htet Aung <nayhtetaung201733@gmail.com>
2023-02-11 22:06:24 +0630

    Reacting to Param Changes

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 <template>
   <TheNavigation />
   <div class="container">
-    <router-view></router-view>
+    <router-view :key="$route.path"></router-view>
   </div>
 
 </template>
--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,7 +1,8 @@
 <template>
-  <section>
+  <section v-if="destination">
     <h1>{{destination.name}}</h1>
     <div class="destination-details">
+
       <img :src="`/images/${destination.image}`" :alt="destination.name">
       <p>{{destination.description}}</p>
     </div>
@@ -13,15 +14,34 @@ import sourceData from '../data.json'
 
 export default {
   name: "DestinationShow",
+  // data(){
+  //   return {
+  //     destination: null
+  //   }
+  // },
   // props: ['id'],
   computed: {
     destinationId() {
       return parseInt(this.$route.params.id)
     },
-    destination() {
-      return sourceData.destinations.find(destination => destination.id === this.destinationId)
+    destination(){
+      return sourceData.destinations.find(destination => destination.id === this.destinationId);
     }
-  }
+  },
+  // methods:{
+  //   async initData(){
+  //     const response = await fetch(`https://travel-dummy-api.netlify.app/${this.$route.params.slug}.json`)
+  //     this.destination = await response.json();
+  //     console.log('run')
+  //   }
+  // },
+  // async created() {
+  //   console.log('beforeinitData')
+  //
+  //   await this.initData();
+  //   await this.$watch(()=> this.$route.params.id ,this.initData)
+  //   console.log('created')
+  // }
 }
 </script>
 
