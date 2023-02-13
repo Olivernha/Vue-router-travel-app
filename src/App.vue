a3255bdd5ef1707e02172f2246931cf82994456b
Nay Htet Aung <nayhtetaung201733@gmail.com>
2023-02-13 21:06:31 +0630

    route transitions

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,30 @@
 <template>
   <TheNavigation />
   <div class="container">
-    <router-view :key="$route.path"></router-view>
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" :key="$route.path" />
+      </transition>
+    </router-view>
   </div>
 
 </template>
 <script>
 import TheNavigation from "./components/TheNavigation.vue";
 export default {
-  components:{
+  components: {
     TheNavigation
   }
 }
 </script>
-<style scoped>
+<style lang="css">
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
 
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
