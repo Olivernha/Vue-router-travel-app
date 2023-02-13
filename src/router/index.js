328a80839314a88bb1f2319e2c8ebe0b4df276b1
Nay Htet Aung <nayhtetaung201733@gmail.com>
2023-02-13 21:41:42 +0630

    route guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
-
+import sourceData from '@/data.json'
 const routes = [
     {
         path: "/",
@@ -12,6 +12,12 @@ const routes = [
         name: 'destination.show',
         component: () => import('@/views/DestinationShow.vue'),
         props: route => ({ id: parseInt(route.params.id) }),
+        beforeEnter: (to, from) => {
+            const exists = sourceData.destinations.find(destination => destination.id === parseInt(to.params.id))
+            if (!exists) {
+                return { name: 'NotFound' }
+            }
+        },
         children: [
             {
                 path: ':experienceSlug',
